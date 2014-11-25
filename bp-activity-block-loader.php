8a2351d1bd7fa84b41fefc74925b5977645135a5
Lena Stergatou <stergatu@cti.gr>
2014-11-25 15:44:23 +0200

    Update bp-activity-block-loader.php

--- bp-activity-block-loader.php
+++ bp-activity-block-loader.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-  Plugin Name: BuddyPress Block Activity Stream Types
-  Plugin URI: http://wordpress.org/extend/plugins/buddypress-block-activity-stream-types/
+  Plugin Name: BuddyPress Stop Record those Activity Types 
+  Plugin URI: 
   Description: Blocks an activity record (based on types) from being saved to the database
-  Author: rich @etivite
-  Author URI: http://etivite.com
+  Author: @lenasterg, nts on cti
+  Author URI: http://lenasterg.wordpress.org
   License: GNU GENERAL PUBLIC LICENSE 3.0 http://www.gnu.org/licenses/gpl.txt
   Version: 0.6
   Text Domain: bp-activity-block
@@ -17,10 +17,10 @@
 /**
  * load textdomain
  */
-function etivite_bp_activity_block_init() {
+function ls_bp_activity_block_init() {
     require( dirname( __FILE__ ) . '/bp-activity-block.php' );
     if ( file_exists( dirname( __FILE__ ) . '/languages/' . get_locale() . '.mo' ) )
 	load_textdomain( 'bp-activity-block', dirname( __FILE__ ) . '/languages/' . get_locale() . '.mo' );
 }
 
-add_action( 'bp_include', 'etivite_bp_activity_block_init', 88 );
+add_action( 'bp_include', 'ls_bp_activity_block_init', 88 );
